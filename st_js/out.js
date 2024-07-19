d09d628efb248bde80b0b1331c724f33d4354480
한지원 <hanjiwon@hanjiwon-ui-MacBookAir.local>
2024-07-19 11:38:30 +0900

    4일차 자바스크립트

--- st_js/out.js
+++ st_js/out.js
@@ -38,3 +38,66 @@
   } else if(hours >= 12) {
     console.log('오후입니다');
   }
+
+  //if, else if, else
+  if(hours < 6 && hours > 1) {
+    console.log('잠을 자렴..');
+  }else if(hours >= 6 && hours < 18) {
+    console.log('공부해라');
+  }else {
+    console.log('쉬어라');
+  }
+
+  //성적 확인
+  let a = 95;
+  if(a > 90) {
+    console.log('A');
+  }else if(a <= 90 && a > 80) {
+    console.log('B');
+  }else if(a <= 80 && a > 70) {
+    console.log('C');
+  }else {
+    console.log('D');
+  }
+
+  //month 사용 조건문 
+  if(3 <= month && month <= 5) {
+    console.log('봄봄봄');
+  }else if(6 <= month && month <= 8) {
+    console.log('습한여름');
+  }else if(9 <= month && month <= 11) {
+    console.log('가을');
+  }else {
+    console.log('눈이 펑펑 겨울');
+  }
+
+  //반복문
+  //for문
+  for (let i = 0; i < 10; i++) {
+    console.log('*');
+  }
+  //while문
+  let j = 0;
+  let koean = ['가', '나', '다'];
+  while (j < koean.length) {
+    console.log(j + '번째 출력: ' + koean[j]);
+
+    j++;
+  }
+
+  //배열 출력
+  let array = [123, '문자', true, { }, {}, [32, 10]];
+  console.log(array);
+  
+  //index 지정해서 출력
+  console.log(array[0]);
+
+  let names = ['지', '한', '원'];
+  names[3] = '94';
+  console.log(names[3]);
+  console.log(names[1]);
+  console.log(names[0]);
+  console.log(names[2]);
+
+  //배열의 요소 갯수
+  console.log(names.length);
