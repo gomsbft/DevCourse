383c91b4f98d4a2d35e5087daf03ef96df33fa98
한지원 <hanjiwon@hanjiwon-ui-MacBookAir.local>
2024-07-19 10:44:33 +0900

    4일차 자바스크립트

--- st_js/out.js
+++ st_js/out.js
@@ -11,8 +11,30 @@
   console.log(20 < 10);
   console.log(20 > 10);
   console.log(20 === 20);
-  let pi;
-  pi = 3.14159265;
+  const pi = 3.14159265;
   console.log(pi);
   let radius = 10;
   console.log(2 * radius * pi);
+// 조건문 if
+  if(5 > 3) {
+    console.log('정답입니다.');
+  }
+
+//날짜, 시간 가져오기
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDay();
+  let hours = date.getHours();
+  let mminutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  console.log(date);
+  console.log(year);
+
+  //오전, 오후 구분하기
+  if(hours < 12) {
+    console.log('오전입니다');
+  } else if(hours >= 12) {
+    console.log('오후입니다');
+  }
