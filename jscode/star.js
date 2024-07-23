2503d24fe2935e93c5c7f9298a0868297eef69c3
한지원 <gomdeschool@naver.com>
2024-07-23 22:56:41 +0900

    6일차

--- jscode/star.js
+++ jscode/star.js
@@ -7,12 +7,14 @@ for(let i = 0; i < 5; i++) {
   star += '*';
 }
 
-console.log(star); */
+console.log(star); 
+*/
 
 /* star 2번
 for(let i = 0; i < 5; i++) {
   console.log('*');
-} */
+} 
+*/
 
 /* star 3번
 for(let i = 0; i < 5; i++) {
@@ -22,17 +24,17 @@ for(let i = 0; i < 5; i++) {
 }
 */
 
-
 /* star 4번
-
 let star = "";
 
 for(let i = 0; i < 5; i++) {
   star += '*';
 
   console.log(star);
-} */
+} 
+*/
 
+/* star 5번
 let star = "";
 for(let i = 0; i < 5; i++) {
   for(let j = 5; j > i; j--) {
@@ -41,6 +43,22 @@ for(let i = 0; i < 5; i++) {
   star += '\n'
 }
 console.log(star);
+*/
+
+
+let result = "";
+
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 5 - i; j++) {
+    result += " ";
+  }
+  for (let k = 1; k <= i; k++) {
+    result += "*";
+  }
+  result += "\n";
+}
+console.log(result);
+
 
 /* star 8번
 let line = 5;
