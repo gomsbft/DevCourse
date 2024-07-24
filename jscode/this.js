6b01f90cccfab187b2cd6d1aafff04c4aa99f930
한지원 <gomdeschool@naver.com>
2024-07-24 09:49:44 +0900

    7일차

--- jscode/this.js
+++ jscode/this.js
@@ -3,8 +3,27 @@
 const pet = {
   name: '구름',
   eat: function(food) {
-    console.log(this.name + ' 은/는 ' + food + ' 을/를 먹습니다.');
+    console.log(this.name + '은/는 ' + food + '을/를 먹습니다.');
   }
 }
 
 pet.eat('밥');
+
+const student = {};
+student.이름 = '한지원'
+student.장래희망 = '개발자'
+student.취미 = '육아'
+
+console.log(JSON.stringify(student, null, 2));
+
+const test = {
+  a: function() {
+    console.log(this);
+  },
+  b:() =>{
+    console.log(this);
+  }
+}
+
+test.a();
+test.b();
