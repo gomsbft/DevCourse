2aec2daaa37f293e1939b3380225c02c021db896
한지원 <gomdeschool@naver.com>
2024-07-25 14:23:36 +0900

    8일차

--- Even/local.js
+++ Even/local.js
@@ -5,10 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const input = document.querySelector('input');
   const button = document.querySelector('button');
 
+  button.style.color = '#faab23'
+  input.style.backgroundColor = '#FAD89E'
+  p.style.color = '#7774b6'
+
+
   const saveValue = localStorage.getItem('input');
   if(saveValue) {
     input.value = saveValue;
-    p.textContent = `이전 실행 때의 마지막 값: ${saveValue}`
+    p.textContent = `마지막 값: ${saveValue}`
   }
 
   input.addEventListener('keyup', (e) => {
