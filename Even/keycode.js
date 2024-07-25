9bbd1a8633bdea5f89c2ddaaeb39e7cc3533e2b2
한지원 <gomdeschool@naver.com>
2024-07-25 09:29:43 +0900

    8일차

---
+++ Even/keycode.js
@@ -0,0 +1,17 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+  const h1 = document.querySelector('h1');
+  const print = (e) => {
+    let output = "";
+    output += `alt: ${e.altKey}<br>`
+    output += `ctrl: ${e.ctrlKey}<br>`
+    output += `shift: ${e.shiftKey}<br>`
+    output += `code: ${typeof(e.code) !== 'undefined' ? e.code : e.keyCode}<br>`
+    h1.innerHTML = output
+  }
+
+  document.addEventListener('keydown', print)
+  document.addEventListener('keyup', print)
+
+})
