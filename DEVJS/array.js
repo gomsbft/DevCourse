dfd71d1aab88ab151de33cc2609ee2ffa8cce188
한지원 <gomdeschool@naver.com>
2024-07-30 11:12:15 +0900

    js

--- DEVJS/array.js
+++ DEVJS/array.js
@@ -83,14 +83,50 @@ let carSales = [300, 350, 400, 450, 500, 600, 650, 620, 580, 620, 580, 620];
 //   year : '2024'
 // }
 
-// for(let prop in car) {
+// for(let prop of car) {
 //   console.log(prop);
 // }
 
-let sum = 0;
-carSales.forEach(sale => {sum += sale;});
+// let sum = 0;
+// carSales.forEach(sale => {sum += sale;});
+// let average = sum / carSales.length;
+// console.log(average);
+
+// // 요소 마다 50씩 더하기
+// carSales.forEach((sale, index, array) => {array[index] = sale + 50});
+// console.log(carSales);
+
+// 배열 메서드
+
+//map()
+// let newCarSales = carSales.map(sale => sale + 50);
+// console.log(newCarSales);
+// console.log(carSales);
+
+//filter()
+// let highSales = carSales.filter(sale => sale > 500);
+// console.log(highSales);
+
+// let evenSales = carSales.filter((sale, index) => index % 2 == 1);
+// console.log(evenSales);
+
+/// //find(), findIndex() 처음 찾은 요소와 처음 찾은 요소의 인덱스
+// let firstSaleSix = carSales.find(sale => sale > 600);
+// console.log(firstSaleSix);
+// let firstSaleSixIndex = carSales.findIndex(sale => sale > 600);
+// console.log(firstSaleSixIndex);
+
+//every() 모든 배열 요소에 대한 조건연산에서의 결과 반환(true, false), 
+//some() 모든 배열 요소에서 하나라도 참 또는 거짓을 반환하는 경우 결과 반환(true, false)
+
+// let allSaleSix = carSales.every(sale => sale > 600);
+// console.log(allSaleSix);
+// let anySaleSix = carSales.some(sale => sale > 600);
+// console.log(anySaleSix);
+
+//reduce()
+
+let sum = carSales.reduce((t1, t2) => t1 + t2, 0);
+console.log(sum);
 let average = sum / carSales.length;
 console.log(average);
-
-carSales.forEach((sale, index, array) => {array[index] = sale + 50});
-console.log(carSales);
