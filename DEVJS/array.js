cca5cbb85b65e9510387b64b6a5e5c3e931f4ae6
한지원 <gomdeschool@naver.com>
2024-07-31 11:51:03 +0900

    js

--- DEVJS/array.js
+++ DEVJS/array.js
@@ -230,19 +230,19 @@
 // }
 // console.log(arr);
 
-// let arr = Array.from(new Array(2), () => new Array(3));
-// console.log(arr);
+let arr = Array.from(new Array(2), () => new Array(3));
+console.log(arr);
 
-// arr[0][0] = 1;
-// arr[0][1] = 2;
-// arr[0][2] = 3;
-// arr[1][0] = 4;
-// arr[1][1] = 5;
-// arr[1][2] = 6;
-// console.table(arr);
+arr[0][0] = 1;
+arr[0][1] = 2;
+arr[0][2] = 3;
+arr[1][0] = 4;
+arr[1][1] = 5;
+arr[1][2] = 6;
+console.table(arr);
 
-// for(let i = 0; i < arr.length; ++i) {
-//   for(let j = 0; j < arr[i].length; ++j) {
-//     console.log(arr[i][j]);
-//   }
-// }
+for(let i = 0; i < arr.length; ++i) {
+  for(let j = 0; j < arr[i].length; ++j) {
+    console.log(arr[i][j]);
+  }
+}
