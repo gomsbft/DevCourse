fa2276a43c5091292e2c2d97b053658e2dd9f9ec
한지원 <gomdeschool@naver.com>
2024-07-31 10:37:42 +0900

    js

---
+++ DEVJS/callback.js
@@ -0,0 +1,31 @@
+// function callback() {
+//   console.log('콜백 함수 호출');
+// }
+// setTimeout(callback, 1000);
+// console.log('작업 수행');
+
+// function getUsers(callback) {
+//   setTimeout(() => {
+//     callback([
+//       {name: 'Kim', email: 'Kim@naver.com'},
+//       {name: 'Lee', email: 'Lee@naver.com'},
+//       {name: 'Park', email: 'Park@naver.com'},
+//     ]);
+//   }, 1000)
+// }
+// function findUser(name, callback) {
+//   getUsers (users => {
+//     const user = users.find(user => user.name === name);
+//     callback(user.email);
+//   });
+// }
+
+// let user = findUser("Kim", user => {
+//   console.log(user);
+// });
+
+let greeting = document.querySelector('#greeting');
+greeting.addEventListener("click", sayHello);
+function sayHello() {
+  console.log("안녕하세요");
+}
--- DEVJS/generate.js
+++ DEVJS/generate.js
@@ -52,15 +52,28 @@ class Sequence {
   }
 }
 
-let evenNumbers = new Sequence(2, 10, 2);
+// let evenNumbers = new Sequence(2, 10, 2);
 // for(const num of evenNumbers) {
 //   console.log(num);
 // }
 
+// function* generateIterables(...iterables) {
+//   for(let iterable of iterables) {
+//      for(let itme of iterable) {
+//       yield itme;
+//      }
+//   }
+// }
+
+
 function* generateIterables(...iterables) {
   for(let iterable of iterables) {
-     for(let itme of iterable) {
-      yield itme;
-     }
+    yield* iterable
   }
 }
+
+
+let evenNumbers = new Sequence(2, 10, 2);
+let generator = generateIterables("abc", [1, 2, 3]);
+let arr = [...generator];
+console.log(arr);
