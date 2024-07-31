c43d29451967e3ca209ee276fd3188ff1eb307b2
한지원 <gomdeschool@naver.com>
2024-07-31 09:43:06 +0900

    js

--- DEVJS/generate.js
+++ DEVJS/generate.js
@@ -24,14 +24,43 @@
 // let iterator = gen[Symbol.iterator]();
 // console.log(iterator);
 
-function* sequence(from = 0, to = Infinity, interval = 1) {
-  let next = from;
-  while(next <= to) {
-    yield next;
-    next += interval;
+// function* sequence(from = 0, to = Infinity, interval = 1) {
+//   let next = from;
+//   while(next <= to) {
+//     yield next;
+//     next += interval;
+//   }
+// }
+// let evenSeq = sequence(2, 10, 2);
+// for(let seq of evenSeq) {
+//   console.log(seq);
+// }
+
+//[Symbol.iterator]() 메서드를 제너레이터 메서드로 구현
+class Sequence {
+  constructor(from = 0, to = Infinity, interval = 1) {
+    this.from = from;
+    this.to = to;
+    this.interval =interval;
+  }
+  *[Symbol.iterator]() {
+    let num = this.from;
+    while(num <= this.to) {
+      yield num;
+      num += this.interval;
+    }
   }
 }
-let evenSeq = sequence(1, 10, 2);
-for(let seq of evenSeq) {
-  console.log(seq);
+
+let evenNumbers = new Sequence(2, 10, 2);
+// for(const num of evenNumbers) {
+//   console.log(num);
+// }
+
+function* generateIterables(...iterables) {
+  for(let iterable of iterables) {
+     for(let itme of iterable) {
+      yield itme;
+     }
+  }
 }
