328fb59f177033391feefeccccf3a8b216f476f1
한지원 <gomdeschool@naver.com>
2024-08-02 10:47:58 +0900

    js

---
+++ DEVJS/dandd.js
@@ -0,0 +1,19 @@
+//드래그 & 드롭
+const draggables = document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".container");
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
+
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging");
+  })
+})
+
+containers.forEach((container) => {
+  container.addEventListener("dragover", () => {
+    const draggable = document.querySelector(".dragging");
+  })
+})
