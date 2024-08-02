81b2a898e51014a1045b72275f4863be607e682c
한지원 <gomdeschool@naver.com>
2024-08-02 11:26:17 +0900

    js

--- DEVJS/dandd.js
+++ DEVJS/dandd.js
@@ -13,7 +13,30 @@ draggables.forEach((draggable) => {
 })
 
 containers.forEach((container) => {
-  container.addEventListener("dragover", () => {
+  container.addEventListener("dragover", (e) => {
+    const afterElement = getDragAferElement(container, e.clientX);
     const draggable = document.querySelector(".dragging");
+    if(afterElement === undefined) {
+      container.appendChild(draggable)
+    }else {
+      container.insertBefore(draggable, afterElement);
+    }
   })
 })
+
+function getDragAferElement(container, x) {
+  const draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.dragging")
+  ];
+
+  return draggableElements.reduce((child) => {
+    const box = child.getBoundingClientRect();
+    const offset = x - box.left - box.width / 2;
+    if(offset < 0 && offset > closeset.offset) {
+      return { offset: offset, element: child};
+    }else {
+      return closeset;
+    }
+
+  }, {offset: Number.NEGATIVE_INFINITY}).element;
+}
