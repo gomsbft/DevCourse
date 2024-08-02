0e0a9d83e69ec928b7e3247941ffada244902e37
한지원 <gomdeschool@naver.com>
2024-08-02 16:03:51 +0900

    js

---
+++ DEVJS/regexp.js
@@ -0,0 +1,10 @@
+let str1 = "1 12 1222 123 1234 1234541 1231 121 1122"
+let pattern = /\d{4,5}/g;
+let pattern2 = /e\B/g;
+let result = str1.match(pattern);
+let result2 = str1.replace(pattern2, "_");
+
+let text = "";
+text += result + '<br>';
+text += result2 + '<br>';
+document.getElementById("show").innerHTML = text;
