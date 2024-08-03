62c8990040c1e108743dc4e5e064483aa758e9e1
한지원 <gomdeschool@naver.com>
2024-08-03 21:02:16 +0900

    js

--- DEVJS/spread.js
+++ DEVJS/spread.js
@@ -63,6 +63,6 @@
 // 객체 person을 만들고 name과 age를 구조 분해 할당하여 변수 userName과 userAge에 각각 저장한 후
 // 두변수를 활용하여 아래와 같은 형식의 문자열을 출력하는 코드를 작성하시오
 
-// const person = {name: 'jiwon', age: 30};
-// const {name: userName, age: userAge} = person;
-// console.log(`이름: ${userName} 나이: ${userAge}`);
+const person = {name: 'jiwon', age: 30};
+const {name: userName, age: userAge} = person;
+console.log(`이름: ${userName} 나이: ${userAge}`);
