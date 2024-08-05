ac863131a66473d4da45a87ad38e5dc4eb954099
한지원 <gomdeschool@naver.com>
2024-08-05 12:50:01 +0900

    js

---
+++ main.js
@@ -0,0 +1,98 @@
+import _ from "lodash"
+
+const user = {
+  name: "mini",
+  age : 30,
+  email: ["123@naver.com"]
+}
+// const copyUser = user;
+// console.log(copyUser === user);
+
+// user.age = 20
+// console.log(user);
+// console.log(copyUser);
+
+// const copyUser = Object.assign({}, user);
+// console.log(user === copyUser);
+
+// user.age = 20
+// console.log(user);
+// console.log(copyUser);
+
+// const copyUser = {...user}
+// console.log(copyUser === user);
+
+// user.age = 20
+// console.log(user);
+// console.log(copyUser);
+
+// user.email.push("han@naver.com")
+// console.log(user.email === copyUser.email)
+
+// console.log(user);
+// console.log(copyUser);
+
+// const copyUser = _.cloneDeep(user);
+// console.log(copyUser === user);
+
+// user.age = 20;
+// console.log(user);
+// console.log(copyUser);
+
+// user.email.push('kim@naver.com');
+// console.log(user.email === copyUser.email);
+
+// const userA = [
+//   {userID: '1', name: 'mini'},
+//   {userID: '2', name: 'big'}
+// ]
+// const userB = [
+//   {userID: '3', name: 'han'},
+//   {userID: '4', name: 'kim'}
+// ]
+
+// const userC = userA.concat(userB);
+// console.log(userC);
+// console.log(_.uniqBy(userC, 'userID'));
+
+// const userD = _.unionBy(userA, userB, "userID");
+// console.log(userD)
+
+// const users = [
+//   {userId: '1', name: 'mini'},
+//   {userId: '2', name: 'big'},
+//   {userId: '3', name: 'kim'},
+//   {userId: '4', name: 'han'}
+// ]
+
+// const foundUser = _.find(users, {name: 'han'});
+// console.log(foundUser);
+// const foundUserIndex = _.findIndex(users, {name: 'han'});
+// console.log(foundUserIndex);
+// _.remove(users, {name: 'mini'});
+// console.log(users);
+
+// let array = [1, 2, 3, 4];
+// let evens = _.remove(array, function(n) {
+//   return n % 2 === 0
+// })
+// console.log(array);
+// console.log(evens);
+
+// let myFriend = [
+//   {name: 'kim', active: false},
+//   {name: 'Lee', active: false}
+// ]
+
+// console.log(_.every(myFriend, {name: 'kim', active: false}));
+// console.log(_.every(myFriend, ['active', false]));
+// console.log(_.every(myFriend, 'active'));
+
+let mySister = [
+  {name: 'kim', age: 22, city: 'seoul'},
+  {name: 'Lee', age: 22, city: 'Daegu'},
+  {name: 'Lee', age: 24, city: 'Busan'},
+  {name: 'Park', age: 25, city: 'seoul'}
+]
+console.log(_.filter(mySister, {age: 22, name: 'kim'}));
+console.log(_.filter(mySister, sister => sister.age === 22));
